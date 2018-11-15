fd290bcd9499664bff2edcdb613db6a3c43d6add
pubby <ruanchunping@gmail.com>
2018-11-15 13:14:23 +0800

    typescript支持

--- ga.d.ts
+++ ga.d.ts
@@ -1,221 +1,226 @@
-interface Map<T> {
-    [key: string]: T
-}
+declare module 'wxapp-ga' {
 
-export class GoogleAnalytics {
-    constructor(app: any)
 
-    setAppName(name: string): GoogleAnalytics
-
-    setAppVersion(version: string): GoogleAnalytics
-
-    getDefaultTracker(): Tracker
-
-    newTracker(trackingID: string): Tracker
-
-    setLog(flag: boolean): GoogleAnalytics
-
-    static getInstance(app: any): GoogleAnalytics
-}
-
-export class Tracker {
-    constructor(ga: GoogleAnalytics, tid: string)
-
-    setTrackerServer(server: string): Tracker
-
-    get(name: string): number | string
-
-    set(name: string, value: number | string): Tracker
-
-    setAnonymizeIp(anonymize: boolean): Tracker
-
-    setAppId(appId: string): Tracker
-
-    setAppInstallerId(appInstallerId: string): Tracker
-
-    setAppName(appName: string): Tracker
-
-    setAppVersion(appVersion: string): Tracker
-
-    setCampaignParamsOnNextHit(uri: string): Tracker
-
-    setClientId(clientId: string): Tracker
-
-    setEncoding(encoding: string): Tracker
-
-    setLanguage(language: string): Tracker
-
-    setLocation(location: string): Tracker
-
-    setScreenColors(screenColors: string): Tracker
-
-    setScreenName(screenName: string): Tracker
-
-    setScreenResolution(width: number, height: number): Tracker
-
-    setViewportSize(viewportSize: string): Tracker
-
-    send(hit: Map<string | number>): Tracker
-}
-
-export class Product {
-    setBrand(brand: string): Product
-
-    setCategory(category: string): Product
-
-    setCouponCode(couponCode: string): Product
-
-    setCustomDimension(index: number, value: string): Product
-
-    setCustomMetric(index: number, value: number): Product
-
-    setId(id: string): Product
-
-    setName(name: string): Product
-
-    setPosition(position: number): Product
-
-    setPrice(price: number): Product
-
-    setQuantity(quantity: number): Product
-
-    setVariant(variant: number): Product
-
-}
-
-export class Promotion {
-    constructor()
-
-    static ACTION_CLICK: string
-    static ACTION_VIEW: string
-
-    setCreative(creative: string): Promotion
-
-    setId(id: string): Promotion
-
-    setName(name: string): Promotion
-
-    setPosition(positionName: string): Promotion
-}
-
-export class ProductAction {
-    constructor()
-
-    static ACTION_ADD: string
-    static ACTION_CHECKOUT: string
-    static ACTION_CHECKOUT_OPTION: string
-    // @Deprecated use ACTION_CHECKOUT_OPTION
-    // ProductAction.ACTION_CHECKOUT_OPTIONS = "checkout_options";
-    static ACTION_CLICK: string
-    static ACTION_DETAIL: string
-    static ACTION_PURCHASE: string
-    static ACTION_REFUND: string
-    static ACTION_REMOVE: string
-
-    setCheckoutOptions(options: string): ProductAction
-
-    setCheckoutStep(step: number): ProductAction
-
-    setProductActionList(productActionList: string): ProductAction
-
-    setProductListSource(productListSource: string): ProductAction
-
-    setTransactionAffiliation(transactionAffiliation: string): ProductAction
-
-    setTransactionCouponCode(transactionCouponCode: string): ProductAction
-
-    setTransactionId(transactionId: string): ProductAction
-
-    setTransactionRevenue(revenue: number): ProductAction
-
-    setTransactionShipping(shipping: number): ProductAction
-
-    setTransactionTax(tax: number): ProductAction
-}
-
-export class CampaignParams {
-    constructor()
-
-    set(name: string, value: string | number): CampaignParams
-
-    toUrl(): string
-
-    static parseFromPageOptions(options: Map<string>, map: Map<string>): CampaignParams
-
-    static buildFromWeappScene(scene: number): CampaignParams
-
-    static parseFromUrl(url: string): CampaignParams
-}
-
-declare namespace HitBuilders {
-
-    export class HitBuilder {
-        get(name: string): number | string
-
-        set(name: string, value: number | string): HitBuilder
-
-        setAll(params: any): HitBuilder
-
-        addImpression(product: Product, impressionList: string): HitBuilder
-
-        addProduct(product: Product): HitBuilder
-
-        addPromotion(promotion: Promotion): HitBuilder
-
-        setProductAction(action: ProductAction): HitBuilder
-
-        setPromotionAction(action: string): HitBuilder
-
-        setCampaignParamsFromUrl(url: string): HitBuilder
-
-        setCustomDimension(index: number, dimension: string): HitBuilder
-
-        setCustomMetric(index: number, metric: number): HitBuilder
-
-        setNewSession(): HitBuilder
-
-        setNonInteraction(nonInteraction: boolean): HitBuilder
-
-        setHitType(hitType: string): HitBuilder
-
-        build(): Map<string | number>
+    interface Map<T> {
+        [key: string]: T
     }
 
+    export class GoogleAnalytics {
 
-    export class ScreenViewBuilder extends HitBuilder {
+        public static getInstance(app: any): GoogleAnalytics
+        constructor(app: any)
+
+        public setAppName(name: string): GoogleAnalytics
+
+        public setAppVersion(version: string): GoogleAnalytics
+
+        public getDefaultTracker(): Tracker
+
+        public newTracker(trackingID: string): Tracker
+
+        public setLog(flag: boolean): GoogleAnalytics
     }
 
-    export class EventBuilder extends HitBuilder {
-        setCategory(category: string): EventBuilder
+    export class Tracker {
+        constructor(ga: GoogleAnalytics, tid: string)
 
-        setAction(action: string): EventBuilder
+        public setTrackerServer(server: string): Tracker
 
-        setLabel(label: string): EventBuilder
+        public get(name: string): number | string
 
-        setValue(value: number): EventBuilder
+        public set(name: string, value: number | string): Tracker
+
+        public setAnonymizeIp(anonymize: boolean): Tracker
+
+        public setAppId(appId: string): Tracker
+
+        public setAppInstallerId(appInstallerId: string): Tracker
+
+        public setAppName(appName: string): Tracker
+
+        public setAppVersion(appVersion: string): Tracker
+
+        public setCampaignParamsOnNextHit(uri: string): Tracker
+
+        public setClientId(clientId: string): Tracker
+
+        public setEncoding(encoding: string): Tracker
+
+        public setLanguage(language: string): Tracker
+
+        public setLocation(location: string): Tracker
+
+        public setScreenColors(screenColors: string): Tracker
+
+        public setScreenName(screenName: string): Tracker
+
+        public setScreenResolution(width: number, height: number): Tracker
+
+        public setViewportSize(viewportSize: string): Tracker
+
+        public send(hit: Map<string | number>): Tracker
     }
 
-    export class SocialBuilder extends HitBuilder {
-        setNetwork(network: string): SocialBuilder
+    export class Product {
+        public setBrand(brand: string): Product
 
-        setAction(action: string): SocialBuilder
+        public setCategory(category: string): Product
 
-        setTarget(target: string): SocialBuilder
+        public setCouponCode(couponCode: string): Product
+
+        public setCustomDimension(index: number, value: string): Product
+
+        public setCustomMetric(index: number, value: number): Product
+
+        public setId(id: string): Product
+
+        public setName(name: string): Product
+
+        public setPosition(position: number): Product
+
+        public setPrice(price: number): Product
+
+        public setQuantity(quantity: number): Product
+
+        public setVariant(variant: number): Product
+
     }
 
-    export class ExceptionBuilder extends HitBuilder {
-        setDescription(description: string): ExceptionBuilder
+    export class Promotion {
 
-        setFatal(is_fatal: bool): ExceptionBuilder
+        public static ACTION_CLICK: string
+        public static ACTION_VIEW: string
+        constructor()
+
+        public setCreative(creative: string): Promotion
+
+        public setId(id: string): Promotion
+
+        public setName(name: string): Promotion
+
+        public setPosition(positionName: string): Promotion
     }
 
-    export class TimingBuilder extends HitBuilder {
-        setCategory(category: string): TimingBuilder
+    export class ProductAction {
 
-        setVariable(variable: string): TimingBuilder
+        public static ACTION_ADD: string
+        public static ACTION_CHECKOUT: string
+        public static ACTION_CHECKOUT_OPTION: string
+        // @Deprecated use ACTION_CHECKOUT_OPTION
+        // ProductAction.ACTION_CHECKOUT_OPTIONS = "checkout_options";
+        public static ACTION_CLICK: string
+        public static ACTION_DETAIL: string
+        public static ACTION_PURCHASE: string
+        public static ACTION_REFUND: string
+        public static ACTION_REMOVE: string
+        constructor(action: string)
 
-        setValue(value: number): TimingBuilder
+        public setCheckoutOptions(options: string): ProductAction
 
-        setLabel(label: string): TimingBuilder
+        public setCheckoutStep(step: number): ProductAction
+
+        public setProductActionList(productActionList: string): ProductAction
+
+        public setProductListSource(productListSource: string): ProductAction
+
+        public setTransactionAffiliation(transactionAffiliation: string): ProductAction
+
+        public setTransactionCouponCode(transactionCouponCode: string): ProductAction
+
+        public setTransactionId(transactionId: string): ProductAction
+
+        public setTransactionRevenue(revenue: number): ProductAction
+
+        public setTransactionShipping(shipping: number): ProductAction
+
+        public setTransactionTax(tax: number): ProductAction
     }
+
+    export class CampaignParams {
+
+        public static parseFromPageOptions(options: Map<string>, map: Map<string>): CampaignParams
+
+        public static buildFromWeappScene(scene: number): CampaignParams
+
+        public static parseFromUrl(url: string): CampaignParams
+        constructor()
+
+        public set(name: string, value: string | number): CampaignParams
+
+        public toUrl(): string
+    }
+
+    export namespace HitBuilders {
+
+        export class HitBuilder {
+            public get(name: string): number | string
+
+            public set(name: string, value: number | string): HitBuilder
+
+            public setAll(params: any): HitBuilder
+
+            public addImpression(product: Product, impressionList: string): HitBuilder
+
+            public addProduct(product: Product): HitBuilder
+
+            public addPromotion(promotion: Promotion): HitBuilder
+
+            public setProductAction(action: ProductAction): HitBuilder
+
+            public setPromotionAction(action: string): HitBuilder
+
+            public setCampaignParamsFromUrl(url: string): HitBuilder
+
+            public setCustomDimension(index: number, dimension: string): HitBuilder
+
+            public setCustomMetric(index: number, metric: number): HitBuilder
+
+            public setNewSession(): HitBuilder
+
+            public setNonInteraction(nonInteraction: boolean): HitBuilder
+
+            public setHitType(hitType: string): HitBuilder
+
+            public build(): Map<string | number>
+        }
+
+
+        export class ScreenViewBuilder extends HitBuilder {
+        }
+
+        export class EventBuilder extends HitBuilder {
+            public setCategory(category: string): EventBuilder
+
+            public setAction(action: string): EventBuilder
+
+            public setLabel(label: string): EventBuilder
+
+            public setValue(value: number): EventBuilder
+        }
+
+        export class SocialBuilder extends HitBuilder {
+            public setNetwork(network: string): SocialBuilder
+
+            public setAction(action: string): SocialBuilder
+
+            public setTarget(target: string): SocialBuilder
+        }
+
+        export class ExceptionBuilder extends HitBuilder {
+            public setDescription(description: string): ExceptionBuilder
+
+            public setFatal(is_fatal: boolean): ExceptionBuilder
+        }
+
+        export class TimingBuilder extends HitBuilder {
+            public setCategory(category: string): TimingBuilder
+
+            public setVariable(variable: string): TimingBuilder
+
+            public setValue(value: number): TimingBuilder
+
+            public setLabel(label: string): TimingBuilder
+        }
+    }
+
 }
